e4fc39ffb32cb3da4ff8b9e5658f4f29e7edf8f8
unknown <yavtushenkon@gmail.com>
2025-01-11 13:08:19 +0200

    tasc4

--- src/components/CountryList/CountryList.jsx
+++ src/components/CountryList/CountryList.jsx
@@ -1,14 +1,15 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { routes } from '../../routes';
 import Grid from '../Grid/Grid';
 import GridItem from '../GridItem/GridItem';
 
 const CountryList = ({ countries }) => {
+  const location = useLocation();
   return (
     <Grid>
       {countries.map(({ id, country, flag }) => (
         <GridItem key={id}>
-          <Link to={`${routes.country}/${id}`}>
+          <Link to={`${routes.country}/${id}`} state={location}>
             <img src={flag} alt={country} />
           </Link>
         </GridItem>
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,12 +1,47 @@
+import { useEffect, useRef, useState } from 'react';
 import Container from '../components/Container/Container';
 import Heading from '../components/Heading/Heading';
 import Section from '../components/Section/Section';
+import { useLocation, useParams } from 'react-router-dom';
+import { fetchCountry } from '../service/countryApi';
+import CountryInfo from '../components/CountryInfo/CountryInfo';
+import GoBackBtn from '../components/GoBackBtn/GoBackBtn';
 
 const Country = () => {
+  const [country, setCountry] = useState({});
+  const { countryId } = useParams();
+  const location = useLocation();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const goBackRef = useRef(location.state ?? '/');
+  useEffect(() => {
+    (async function fetchCountries() {
+      try {
+        setIsLoading(true);
+        const data = await fetchCountry(countryId);
+        setCountry(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, [countryId]);
+  console.log(location);
+
   return (
     <Section>
       <Container>
         <Heading title="Country" bottom />
+        <GoBackBtn path={goBackRef.current} />
+        <CountryInfo
+          capital={country.capital}
+          countryName={country.countryName}
+          flag={country.flag}
+          population={country.population}
+          languages={country.languages}
+        />
       </Container>
     </Section>
   );
